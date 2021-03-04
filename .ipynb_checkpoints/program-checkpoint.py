4068d6acbc51e81ddc16e533e74fa3ca919afa7e
Rankin <rat81@pitt.edu>
2021-03-04 00:15:34 -0500

    Added formatting on the readme file

---
+++ .ipynb_checkpoints/program-checkpoint.py
@@ -0,0 +1,8 @@
+name = input("What is your name? ")
+
+inputString = input("Enter a string as input: ")
+# This can change later, depending on what our program actually does
+
+#TODO do something to the input
+
+print("Hello, "+name+". Your input was "+inputString)
