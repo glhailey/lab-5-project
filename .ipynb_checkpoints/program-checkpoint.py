8dbce3054f83b22abeae8001804bf2043b3484d1
Rankin <rat81@pitt.edu>
2021-03-04 00:28:43 -0500

    Removed the hidden jupyter file

--- .ipynb_checkpoints/program-checkpoint.py
+++
@@ -1,8 +0,0 @@
-name = input("What is your name? ")
-
-inputString = input("Enter a string as input: ")
-# This can change later, depending on what our program actually does
-
-#TODO do something to the input
-
-print("Hello, "+name+". Your input was "+inputString)
